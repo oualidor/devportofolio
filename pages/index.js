7e9b4513d9edaeb98808645ee97adf18fcf24f22
Oualid KHIAL <walid.khial@gmail.com>
2022-06-03 14:37:49 +0100

    Adding google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
 
-import {Box, Container} from "theme-ui";
 import {useEffect} from "react";
 import Timeline from '../components/TimeLine/TimeLine';
 import Career from "../sections/Home/Career/Career"
-import ShowRoom from "../sections/Home/ShowRoom/ShowRoom"
 import Landing from "../sections/Home/Landing/Landing"
 import Skills from "../sections/Home/Skills/Skills"
 import Testimonial from "../sections/Home/Testimonial/Testimonial"
 import 'react-multi-carousel/lib/styles.css';
 import 'rc-drawer/assets/index.css';
+import ReactGA from 'react-ga';
+import Router, {useRouter} from 'next/router';
 
+export const initGA = () => {
+    console.log('GA init');
+    ReactGA.initialize('G-8L31KNNS3F');
+};
 
-
+export const logPageView = () => {
+    console.log(`Logging pageview for ${window.location.pathname}`);
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+};
 
 const EducationalBackgroundData = [
     { year: "2009 - 2012", degree: 'High School degree, ', spec:  "Experimengal Scince", school:  "Youcfi Bouchrit High School"},
@@ -29,20 +37,24 @@ const EducationalBackgroundData = [
 
 export default function Home(props) {
 
-    useEffect(()=>{
-    })
+    useEffect(() => {
+
+        initGA();
+        logPageView();
+        Router.events.on('routeChangeComplete', logPageView);
+    }, []);
 
   return (
       <>
-     <Landing></Landing>      
+     <Landing></Landing>
       <Timeline Title={"Educational Background"} Data={EducationalBackgroundData}/>
       <Skills></Skills>
       <Career></Career>
       {/* <ShowRoom></ShowRoom> */}
-     
+
 
       <Testimonial></Testimonial>
-  
+
       </>
   )
 }
