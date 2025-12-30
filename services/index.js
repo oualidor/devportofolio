766ccfade899c9bdcb79b167b662841641801831
Oualidoor <walid.khial@gmail.com>
2025-12-30 18:42:22 +0100

    Show only published entries

--- services/index.js
+++ services/index.js
@@ -3,22 +3,26 @@ import { _SanityClient } from './_SanityClient';
 
 
 export const getCarrier = async () => {
-  const query = `*[_type == "career"] | order(from desc)[0..10]{
+  const query = `*[
+    _type == "career" &&
+    publish == true
+  ]
+  | order(from desc)[0..10]{
     ...,
     company->{..., contactInfo[]{..., type->}},
     projects[]->{..., skills[]->}
   }`;
 
-  let careerData = await _SanityClient.fetch(query);
+  const careerData = await _SanityClient.fetch(query);
   return careerData;
 };
 
 export const getOneCarrier = async (id) => {
-  try{
-    const query = '*[_type == "career" && _id == "'+id+'"][0..1]{...,  company->{..., contactInfo[]{..., type->}}, projects[]->{..., skills[]->,   images[]{asset->{_id, url}},  links[]{..., type->}}}';
-    let careerData  = await _SanityClient.fetch(query)
+  try {
+    const query = '*[_type == "career" && _id == "' + id + '"][0..1]{...,  company->{..., contactInfo[]{..., type->}}, projects[]->{..., skills[]->,   images[]{asset->{_id, url}},  links[]{..., type->}}}';
+    let careerData = await _SanityClient.fetch(query)
     return careerData[0]
-  }catch (e){
+  } catch (e) {
     return e
 
   }
@@ -26,13 +30,13 @@ export const getOneCarrier = async (id) => {
 
 export const getTestimonials = async () => {
   const query = '*[_type == "testimonial"][0..4]{ ..., contactInfo[]{..., type->}}';
-  let careerData  = await _SanityClient.fetch(query)
+  let careerData = await _SanityClient.fetch(query)
   return careerData
 };
 
 export const getPerson = async () => {
   const query = '*[_type == "person" ][0..1]{..., contactInfo[]{..., type->} }';
-  let careerData  = await _SanityClient.fetch(query)
+  let careerData = await _SanityClient.fetch(query)
   return careerData[0]
 };
 
